44c7cdacd9df52843c2682dca5cc8641aba21940
FlyingJetski <joshuaxu71@gmail.com>
2021-02-21 21:15:03 +0700

    updated client.java

--- src/main/java/com/company/client/Client.java
+++ src/main/java/com/company/client/Client.java
@@ -4,6 +4,7 @@ import com.company.common.*;
 import com.company.common.exceptions.DuplicateException;
 import com.company.common.exceptions.MandatoryException;
 import com.company.models.User;
+import com.company.utility.UserUtility;
 
 import java.net.*;
 import java.rmi.*;
@@ -12,5 +13,12 @@ public class Client {
     public static void main(String[] args) throws MalformedURLException, NotBoundException, RemoteException, DuplicateException, MandatoryException {
         RemoteInterface server = (RemoteInterface)Naming.lookup("rmi://localhost:1044/server");
         server.resetDatabase();
+        User admin = new User()
+                .setAdmin(true)
+                .setUsername("admin")
+                .setPassword("password")
+                .setFirstName("Admin")
+                .setIcPassport("ADMIN000");
+        server.insertUser(admin);
     }
 }
